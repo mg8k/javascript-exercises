6deb4ac8e1dc8f6a4f1924ef9a97feebc83363fd
Mohamed Kassem <mohamedgamal657@gmail.com>
2026-07-17 00:49:26 +0000

    solving foundation 12 through 14

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let alphanumeric = "abcdefghijklmnopqrstuvwxyz0123456789";
+    const cleanedWord = word.toLowerCase()
+        .split("")
+        .filter((letter)=> alphanumeric.includes(letter))
+        .join("");
+    const newWord = cleanedWord.split("").reverse().join("");
+    
+    console.log(newWord);
+    return newWord.toLowerCase() === cleanedWord.toLowerCase()
 
-};
-
+}
 // Do not edit below this line
 module.exports = palindromes;
--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let total = 0;
+    for(let i = 0 ; i<num; i++){
+        total += i-1;
+        console.log(total);
+    }
+    return total;
 };
 
 // Do not edit below this line
